d17682a2608fbd39743f3b8e3cbd0601d6c55524
SohamSawantTechturbine <ssawant@techturbine.com>
2024-04-05 18:21:00 +0530

    login and Registration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import './Login.css'
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContextProvider, useAuthContext } from '../context';
-
+import { Link } from 'react-router-dom';
 export const Login = () => {
   // const [isLogin, setIsLogin] = useState(false);
   const [username, setUsername] = useState('');
@@ -45,14 +45,41 @@ export const Login = () => {
       //   <button type='button' onClick={handleRegeister}>Register here</button>
       // </div>
     // </AuthContextProvider>
-    <div className="login-container">
-    <h2>Login</h2>
-    <form className="login-form" onSubmit={handleButton}>
-      <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit">Login</button>
-    </form>
-    <button className="register-button" onClick={handleRegeister}>Register here</button>
+  //   <div className="login-container">
+  //   <h2>Login</h2>
+  //   <form className="login-form" onSubmit={handleButton}>
+  //     <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+  //     <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+  //     <button type="submit">Login</button>
+  //   </form>
+  //   <button className="register-button" onClick={handleRegeister}>Register here</button>
+  // </div>
+  <section className="bg-gray-50 dark:bg-gray-700">
+  <div className="flex flex-col items-center justify-center px-4 py-6 mx-auto md:h-screen lg:py-0">
+     
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleButton}>
+                  <div>
+                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Your name</label>
+                      <input type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com or name" required="" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                  </div>
+                  <div>
+                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="text" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                  </div>
+                
+                  <button type="submit" className="w-full text-white bg-green-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <Link to="register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                  </p>
+              </form>
+          </div>
+      </div>
   </div>
+</section>
   );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -52,9 +52,11 @@
 // }
 
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
 import { AuthContextProvider } from '../context';
 import './Register.css'
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Register = () => {
     const [username, setUsername] = useState('');
@@ -67,16 +69,16 @@ export const Register = () => {
         e.preventDefault()
         if (username.trim()!=="" && password.trim()!==""&&username.trim() && password.trim()) {
             saveData();
-            alert("Registered successfully. You can click on the login button and enjoy.");
+           {toast("Registered successfully. You can click on the login button and enjoy.");}
         } else {
-            alert("Enter both username and password");
+           { toast("Enter both username and password");}
         }
     }
 
     function saveData() {
-        // Append new user data to userData array
+      
         userData.push({ username, password });
-        // Update userData in localStorage
+       
         localStorage.setItem('userData', JSON.stringify(userData));
     }
 
@@ -88,13 +90,55 @@ export const Register = () => {
         // <AuthContextProvider>
            
         // </AuthContextProvider>
-        <div className='register-container'>
-        <form className="register-form" onSubmit={handleButton}>
-            <input type="text" placeholder='Enter your username' value={username} onChange={(e) => setUsername(e.target.value)} />
-            <input type="password" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button type="submit">Register</button><br/>
-        </form>
-        <button className="login-button" type='button' onClick={handleLogin}>Login here</button>
-    </div>);
+    //     <div className='register-container'>
+    //     <form className="register-form" onSubmit={handleButton}>
+    //         <input type="text" placeholder='Enter your username' value={username} onChange={(e) => setUsername(e.target.value)} />
+    //         <input type="password" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} />
+    //         <button type="submit">Register</button><br/>
+    //     </form>
+    //     <button className="login-button" type='button' onClick={handleLogin}>Login here</button>
+    // </div>
+
+<section className="bg-gray-50 dark:bg-gray-900">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Create and account
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleButton}>
+                  <div>
+                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Username</label>
+                      <input type="text" name="email" id="email" value={username} onChange={(e) => setUsername(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                  </div>
+                  <div>
+                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="text" name="password" id="password" placeholder="••••••••"   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+                  <div>
+                      <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                      <input type="text" name="confirm-password" id="confirm-password" placeholder="••••••••" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+                  <div className="flex items-start">
+                      <div className="flex items-center h-5">
+                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                      </div>
+                      <div className="ml-3 text-sm">
+                        <label for="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                      </div>
+                  </div>
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account? <Link to="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
+    
+    );
+    
 };
 
